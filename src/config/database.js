e477b4773a2bd089005806e254b542a599def04d
Sriramula Gowtham <sriramulagowtham115@gmail.com>
2026-02-11 11:35:27 +0530

    Updated for deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
